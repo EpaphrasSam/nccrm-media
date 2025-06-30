323e97e03da40fc902192aa40fef895386549362
EpaphrasSam <isinesam@gmail.com>
2025-06-30 15:39:50 +0000

    feat: added powerbi dashboard

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,24 @@
 export default function Home() {
-  return <div>Home</div>;
+  const powerbiUrl = process.env.POWERBI_EMBED_URL;
+
+  return (
+    <div className="h-screen -my-6 -mx-4 md:-mx-6">
+      {powerbiUrl ? (
+        <iframe
+          title="NCCRM DataHub Analytics"
+          src={powerbiUrl}
+          width="100%"
+          height="100%"
+          allowFullScreen
+          // loading="lazy"
+          // sandbox="allow-scripts allow-same-origin allow-forms allow-downloads allow-popups"
+          // style={{ border: "none" }}
+        />
+      ) : (
+        <div className="flex items-center justify-center h-full">
+          <p>PowerBI dashboard not configured</p>
+        </div>
+      )}
+    </div>
+  );
 }
